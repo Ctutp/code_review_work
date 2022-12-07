9ad9517057be5f3f83c30043bf89a433bf22bc7d
grk_rise <grk.rise@gmail.com>
2022-12-07 17:07:55 +0300

    add source

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public int add(int a, int b){
+        //TODO inser your realisation in method add
+    }
+    public int dif(int a, int b){
+        //TODO inser your realisation in method dif
+    }
+    public int div(int a, int b){
+        //TODO inser your realisation in method div
+    }
+    public int times(int a, int b){
+        //TODO inser your realisation in method times
+    }
+    public int solver(){
+        //TODO inser your realisation in method solver
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        //TODO inser your realisation in test-method add
+    }
+
+    @Test
+    void dif() {
+        //TODO inser your realisation in test-method dif
+    }
+
+    @Test
+    void div() {
+        //TODO inser your realisation in test-method div
+    }
+
+    @Test
+    void times() {
+        //TODO inser your realisation in test-method times
+    }
+
+    @Test
+    void solver() {
+        //TODO inser your realisation in test-method solver
+    }
+}
