f239d05596dcb3ebff2253563def804c36235195
d-a-tacito <dananto02@gmail.com>
2022-12-17 23:50:56 +0300

    Updated Calculator functions

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,22 @@
 public class Calculator {
     public int add(int a, int b){
         //TODO inser your realisation in method add
+        return a+b;
     }
     public int dif(int a, int b){
         //TODO inser your realisation in method dif
+        return a-b;
     }
     public int div(int a, int b){
         //TODO inser your realisation in method div
+        return a/b;
     }
     public int times(int a, int b){
         //TODO inser your realisation in method times
+        return a*b;
     }
-    public int solver(){
+    public int solver(int a,int b,int c){
         //TODO inser your realisation in method solver
+        return (a*b+2023)/c;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,28 +4,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    Calculator calculator=new Calculator();
     @Test
     void add() {
         //TODO inser your realisation in test-method add
+        assertEquals(3,calculator.add(1,2));
     }
 
     @Test
     void dif() {
         //TODO inser your realisation in test-method dif
+        assertEquals(4,calculator.dif(5,1));
     }
 
     @Test
     void div() {
         //TODO inser your realisation in test-method div
+        assertEquals(2,calculator.div(6,3));
     }
 
     @Test
     void times() {
         //TODO inser your realisation in test-method times
+        assertEquals(4,calculator.times(1,4));
     }
 
     @Test
     void solver() {
         //TODO inser your realisation in test-method solver
+        assertEquals(508,calculator.solver(3,3,4));
     }
 }
