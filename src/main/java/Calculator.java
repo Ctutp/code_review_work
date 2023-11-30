ad0d0d0e6d89d299d97131f2a43c14865a4ae2ee
dimitrich212003 <ytrewq212003@mail.ru>
2023-11-30 18:42:00 +0300

    create calculator and solver method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,26 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
+
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
+
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new IllegalArgumentException("Деление на ноль недопустимо");
+        }
     }
+
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver(int a, int b, int c) {
+        int result = div(times(a, b), add(c, dif(b, a)));
+        return result;
     }
 }
