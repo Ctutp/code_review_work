be3bd402c1ffedda75657f16acb4144733031f12
les <adkajsdasduoisaudkksak@dsfalasjkdlajsdk.ru>
2023-12-01 21:00:32 +0300

    make all tasks

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,28 +4,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    Calculator calculator = new Calculator();
+
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(2, calculator.add(1, 1));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(0, calculator.dif(1, 1));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertEquals(1, calculator.div(1, 1));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(3, calculator.times(3, 1));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        assertEquals(76, calculator.solver());
     }
 }
