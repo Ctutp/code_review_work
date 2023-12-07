fe8a25f5ba9f6ecb8584532ae34b600c30ba8444
Your Name <you@example.com>
2023-12-07 17:40:27 +0300

    func realization

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        return a / b;
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    // let equation be a * x = b
+    public int solver(int a, int b) {
+        return div(b, a);
     }
 }
