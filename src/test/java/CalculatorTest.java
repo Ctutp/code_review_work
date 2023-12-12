e8335b2148420b6d84eac4481cbbebacdd7a3e30
Даниил <113928056+gigDani@users.noreply.github.com>
2023-12-12 19:35:56 +0300

    Методы для калькулятора

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,29 +3,36 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calculator = new Calculator();
 
     @Test
     void add() {
         //TODO inser your realisation in test-method add
+        assertEquals(5,calculator.add(2,3));
     }
 
     @Test
     void dif() {
         //TODO inser your realisation in test-method dif
+        assertEquals(2,calculator.dif(5,3));
+
     }
 
     @Test
     void div() {
         //TODO inser your realisation in test-method div
+        assertEquals(4,calculator.div(8,2));
     }
 
     @Test
     void times() {
         //TODO inser your realisation in test-method times
+        assertEquals(6,calculator.times(2,3));
     }
 
     @Test
     void solver() {
         //TODO inser your realisation in test-method solver
+        assertEquals(3,calculator.solver());
     }
 }
