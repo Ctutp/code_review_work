2a5111e19532d71cb619ed709b79a37d3d3b02eb
Timofey <timafolf2003@mail.ru>
2023-12-15 00:22:15 +0300

    change README.md implement methods in Calculator.java, CalculatorTest.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,23 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b == 0){
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+    public int solver(int a, int b){
+        int multiplication = times(a, b);
+        int subtraction = dif(a, b);
+        int result = add(multiplication, subtraction);
+        return result;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,26 +6,36 @@ class CalculatorTest {
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        Calculator calculator = new Calculator();
+        int result = calculator.add(3, 5);
+        assertEquals(8, result);
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        Calculator calculator = new Calculator();
+        int result = calculator.dif(8, 3);
+        assertEquals(5, result);
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        Calculator calculator = new Calculator();
+        int result = calculator.div(10, 2);
+        assertEquals(5, result);
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        Calculator calculator = new Calculator();
+        int result = calculator.times(4, 6);
+        assertEquals(24, result);
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        Calculator calculator = new Calculator();
+        int result = calculator.solver(5, 3);
+        assertEquals(17, result);
     }
 }
