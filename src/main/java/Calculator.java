a058b50fe4b1d4328f926ac60f247a2ec3657649
gfi406 <makafanasiev@gmail.com>
2023-12-14 22:47:39 +0300

    complite TODO

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,42 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
+
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
+
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
     }
+
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver(int a, int b, String operation){
+        int result = 0;
+        switch(operation) {
+            case "add":
+                result = add(a, b);
+                break;
+            case "dif":
+                result = dif(a, b);
+                break;
+            case "div":
+                result = div(a, b);
+                break;
+            case "times":
+                result = times(a, b);
+                break;
+            default:
+                System.out.println("Operation not supported");
+        }
+        return result;
     }
 }
