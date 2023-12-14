68c2bb93e6ba540f7379005772c3b28d58a611a7
TimurU13 <timurkausmanov4@gmail.com>
2023-12-14 22:48:24 +0300

    todo solve

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,25 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver() {
+        int result = add(5, 3);  // Пример использования метода add в методе solver
+        return result;
     }
 }
